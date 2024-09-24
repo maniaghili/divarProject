f3592b2fe2b0b3444ef365532390c28483dae416
maniaghili <aligholimani@gmail.com>
2024-09-24 12:03:13 +0330

    update

--- js/adminPanel/main.js
+++ js/adminPanel/main.js
@@ -1,13 +1,20 @@
 import { isLoginn } from "../funcs/postDetalesfuncs.js";
 import { getToken } from "../funcs/share.js";
+import { logOut } from "../funcs/util.js";
 
 window.addEventListener('load',()=>{
+    const userPhone = document.querySelector('#user-phone')
+    const logout = document.querySelector('.logout')
     if(getToken()){
         isLoginn().then((user)=>{
-            console.log(user.data.user);
-            
+            let userInfo = user.data.user
+            userPhone.innerHTML = userInfo.phone
         })
         
     }
     
+
+    logout.addEventListener('click',()=>{
+        logOut()
+    })
 })
