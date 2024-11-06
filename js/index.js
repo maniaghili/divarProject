2b8a61812ae6459dd293c272462740f2fa8ab2e6
MANI <164733484+maniaghili@users.noreply.github.com>
2024-11-06 23:06:36 +0330

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,7 +1,7 @@
 import { getPopularCities ,redirectUserAccordingToCookie,getAllCities,showCitiesInInput} from "./funcs/indexfuncs.js";
 
 const mainInput = document.querySelector('.main__input')
-
+alert('این پروژه ریسپانسیو نیست و برای تجربه بهتر با ویندوز وارد شوید')
 window.addEventListener('load',()=>{
     redirectUserAccordingToCookie()
     // Other functions are used inside (getPopularCities)
